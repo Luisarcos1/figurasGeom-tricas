09d269862a504b28f9cbdb410f0376813b7ee7a5
Luisarcos1 <168046474+Luisarcos1@users.noreply.github.com>
2024-05-30 11:45:09 -0600

    Add files via upload

---
+++ figurasGeometricas/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace figurasGeometricas
+{
+    public partial class Form1 : Form
+    {
+        private object pictureboxpro;
+
+        public Form1()
+
+            
+        {
+            InitializeComponent();
+        }
+
+        private void TRIANGULO_Click(object sender, EventArgs e)
+        {
+            pictureboxpro.imagenes figuras  = figurasGeometricas.Program.resourse.triangulo;
+
+        }
+    }
+}
---
+++ figurasGeometricas/cuadrado.cs
@@ -0,0 +1,32 @@
+﻿using FiguraGeometrica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FiguraGeometrica
+{
+    class Cuadrado : Figura
+{
+        double lado1;
+
+        public Cuadrado(double lado)
+        {
+            this.Lado1 = Lado1;
+        }
+
+        public override float area()
+        {
+            return Lado1 * Lado1;
+        }
+
+        public override float volumen()
+
+        {
+            return 0; // Los cuadrados no tienen volumen
+
+        }
+    }
+}
